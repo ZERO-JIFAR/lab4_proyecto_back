cc62b74331d688e04a600a11dfdabef9f0e3131c
ZERO-JIFAR <juanignacioarnold@gmail.com>
2025-05-10 16:42:51 -0300

    inicio back

---
+++ src/test/java/com/example/lab4_proyecto_back/Lab4ProyectoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_proyecto_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4ProyectoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
