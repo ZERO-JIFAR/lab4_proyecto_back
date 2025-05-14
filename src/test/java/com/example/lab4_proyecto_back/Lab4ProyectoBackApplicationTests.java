6ee806fe12e68c3f805451e043a704602ba66701
ZERO-JIFAR <juanignacioarnold@gmail.com>
2025-05-14 17:15:52 -0300

    bien creado el proyecto

--- src/test/java/com/example/lab4_proyecto_back/Lab4ProyectoBackApplicationTests.java
+++ src/test/java/com/example/lab4_proyecto_back/Lab4ProyectoBackApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Lab4ProyectoBackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
