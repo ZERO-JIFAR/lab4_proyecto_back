ecd96a40c1b5ce15f4a766faab3bc2d3917a1733
ZERO-JIFAR <juanignacioarnold@gmail.com>
2025-05-14 20:39:05 -0300

    vamos de nuevo -_-

---
+++ src/test/java/com/ecommerce/ecommerce/Lab4ProyectoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4ProyectoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
