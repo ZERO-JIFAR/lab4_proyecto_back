4202921757e5a05e2945f04e656ea44243a4e387
FreshEffect15 <gossuinulises@gmail.com>
2025-06-26 03:05:13 -0300

    Descuentos reales

--- src/main/java/com/example/apirest/entities/Producto.java
+++ src/main/java/com/example/apirest/entities/Producto.java
@@ -17,6 +17,8 @@ public class Producto extends Base {
     private String nombre;
     private Integer cantidad;
     private Double precio;
+    private Double precioOriginal; // <-- NUEVO CAMPO PARA GUARDAR EL PRECIO ANTERIOR
+
     private String descripcion;
     private String color;
     private String marca;
